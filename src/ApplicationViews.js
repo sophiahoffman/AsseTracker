9c68c52c90b8dad6ca44763b4cd6d905c2f18a84
Sophia Hoffman <sophiahoffman@hotmail.com>
2019-12-09 12:32:11 -0600

    populate json example file and set up file structure

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -1,7 +1,21 @@
 
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import Test from './components/Test';
+import PersonalPropertyList from './components/personalProperty/PersonalPropertyList';
+import PersonalPropertyAdd from './components/personalProperty/PersonalPropertyAdd';
+import PersonalPropertyDisposal from './components/personalProperty/PersonalPropertyDisposal';
+import PersonalPropertyEdit from './components/personalProperty/PersonalPropertyEdit';
+import VehiclesList from './components/vehicles/VehiclesList';
+import VehiclesAdd from './components/vehicles/VehiclesAdd';
+import VehiclesDisposal from './components/vehicles/VehiclesDisposal';
+import VehiclesEdit from './components/vehicles/VehiclesEdit';
+import RealEstateList from './components/realEstate/RealEstateList';
+import RealEstateAdd from './components/realEstate/RealEstateAdd';
+import RealEstateDisposal from './components/realEstate/RealEstateDisposal';
+import RealEstateEdit from './components/realEstate/RealEstateEdit';
+import WelcomeAsseTracker from './components/welcomeAsseTracker/WelcomeAsseTracker';
+import Login from './components/loginRegister/Login';
+import Register from './components/loginRegister/Register';
 
 
 
