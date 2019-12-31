9ef83100c79450993aefb95e13d6c0f5984dd76c
Sophia Hoffman <sophiahoffman@hotmail.com>
2019-12-31 03:43:49 -0600

    personal property radio button display

--- src/components/personalProperty/PersonalPropertyList.js
+++ src/components/personalProperty/PersonalPropertyList.js
@@ -2,28 +2,64 @@
 
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import { Radio, RadioGroup} from 'react-radio-group';
 import PersonalPropertyAPIManager from '../../modules/PersonalPropertyAPIManager'
 import PersonalPropertyCard from './PersonalPropertyCard';
+import './PersonalPropertyList.css';
 
 class PersonalPropertyList extends Component {
     state = {
         personalProperty: [],
-        loadingStatus: true
+        loadingStatus: true,
+        selectedValue: "active"
     }
 
     componentDidMount() {
-        this.setPersonalPropertyState()
+            this.setPersonalPropertyState()
     }
 
     setPersonalPropertyState = () => {
         this.setState({loadingStatus: false})
-        PersonalPropertyAPIManager.getAllPersonalProperty()
-        .then(personalProperty => {
-            this.setState({
-            personalProperty: personalProperty,
-            })
-        })
+        if (this.state.selectedValue === "active") {
+            return (
+                PersonalPropertyAPIManager.getActivePersonalProperty()
+                .then(personalProperty => {
+                    this.setState({
+                    personalProperty: personalProperty,
+                    })
+                })
+            )
+        } else if (this.state.selectedValue === "disposed") {
+            return (
+                PersonalPropertyAPIManager.getDisposedPersonalProperty()
+                .then(personalProperty => {
+                    this.setState({
+                    personalProperty: personalProperty,
+                    })
+                })
+            )
+        } else {
+            return (
+                PersonalPropertyAPIManager.getAllPersonalProperty()
+                .then(personalProperty => {
+                    this.setState({
+                    personalProperty: personalProperty,
+                    })
+                })
+            )
+        }
     }
+
+    updatePersonalPropertyState = e => {
+        e.preventDefault();
+        this.setPersonalPropertyState()
+    }
+
+    handleChange = value => {
+    this.setState({selectedValue: value});
+    console.log("selected value", this.state.selectedValue)
+  }
+
      
     deletePersonalProperty = personalPropertyId => {
         this.setState({loadingStatus: true})        
@@ -36,7 +72,24 @@ class PersonalPropertyList extends Component {
             <React.Fragment>
                 <div className="button-new personalProperty-section-content">
                     <Button variant="secondary" type="button" className="newPersonalPropertyBtn" onClick={() => this.props.history.push("personalproperty/new")}>Add New Item</Button>
+                    <hr />
                 </div>
+            <form className="form-radio" onSubmit={this.updatePersonalPropertyState}>
+
+                <RadioGroup className="radio-button-group" name="assetDisplay" selectedValue={this.state.selectedValue} onChange={this.handleChange}>
+                    <label>
+                        <Radio value="active" className="radio-button"  />  Active
+                    </label>
+                    <label>
+                        <Radio value="disposed" className="radio-button"  />  Disposed
+                    </label>
+                    <label>
+                        <Radio value="all" className="radio-button"  />  All
+                    </label>
+                </RadioGroup>
+                <Button variant="secondary" type="submit">Display</Button>
+                </form>
+
                 <div className="personalProperty-container-cards container-cards" align="center">
                     {this.state.personalProperty.map(personalProperty => 
                         <PersonalPropertyCard 
